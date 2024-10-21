05dfe806e550e8324e93a019fadc600a350f437f
Nidhi D B <nidhidb.cs23@bmsce.ac.in>
2024-10-21 20:57:36 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,16 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+
+
+lis1out=[]
+for i in list1:
+    if i>0:
+        lis1out.append(i)
+
+lis2out=[]
+for i in list2:
+    if i>0:
+        lis2out.append(i)
+
+print(lis1out)
+print(lis2out)
